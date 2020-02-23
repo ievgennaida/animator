9e6f943a2774fe2a62f4193275aa1ce87441d2e6
Ievgen Naida <evgenynaida@gmail.com>
2020-02-23 20:46:06 +0100

    more accurate player bounds when svg is loaded.

--- src/app/services/svg/svg-initializer.ts
+++ src/app/services/svg/svg-initializer.ts
@@ -35,7 +35,24 @@ export class SvgInitializer implements IInitializer {
     document.parsedData = svgcontent;
     const results = new InitResults();
     results.player = new SvgPlayer(svgcontent);
-    results.size = svgcontent.getBBox();
+    const w = svgcontent.width.baseVal;
+    const h = svgcontent.height.baseVal;
+    if (
+      !w.valueAsString ||
+      !h.valueAsString ||
+      w.unitType === w.SVG_LENGTHTYPE_PERCENTAGE ||
+      h.unitType === h.SVG_LENGTHTYPE_PERCENTAGE
+    ) {
+      results.size = svgcontent.getBBox();
+    } else {
+      results.size = new DOMRect(
+        svgcontent.x.baseVal.value,
+        svgcontent.y.baseVal.value,
+        w.value,
+        h.value
+      );
+    }
+
     return results;
   }
 }
