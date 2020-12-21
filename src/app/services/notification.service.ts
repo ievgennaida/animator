22d4c38a0756524e2ccb2a74ba420dcaefe1ebac
Ievgen Naida <evgenynaida@gmail.com>
2020-12-21 21:44:56 +0100

    changed outline offset.

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
+/**
+ * Show messages in the player window.
+ */
 @Injectable({
   providedIn: "root",
 })
 export class NotificationService {
-  notificationSubject = new Subject<string>();
+  notificationSubject = new BehaviorSubject<string | null>(null);
   constructor() {}
 
+  hideMessage() {
+    this.notificationSubject.next(null);
+  }
   showMessage(message: string) {
     this.notificationSubject.next(message);
   }
