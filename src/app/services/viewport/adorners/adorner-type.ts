1ea20b0b8d4ba8a975183610dfd1c6e59dbb862b
Ievgen Naida <evgenynaida@gmail.com>
2020-12-30 20:16:24 +0100

    only one entry is allowed for the adorner enum (no mutliselect anymore)

--- src/app/services/viewport/adorners/adorner-type.ts
+++ src/app/services/viewport/adorners/adorner-type.ts
@@ -1,35 +1,32 @@
 // tslint:disable: no-bitwise
 
-import { Utils } from "../../utils/utils";
-
-// Bitwise
 export enum AdornerType {
   None = 0,
 
-  TopLeft = 1 << 0,
-  TopCenter = 1 << 1,
-  TopRight = 1 << 2,
-  BottomLeft = 1 << 3,
-  BottomCenter = 1 << 4,
-  BottomRight = 1 << 5,
-  LeftCenter = 1 << 6,
-  RightCenter = 1 << 7,
+  TopLeft,
+  TopCenter,
+  TopRight,
+  BottomLeft,
+  BottomCenter,
+  BottomRight,
+  LeftCenter,
+  RightCenter,
   /**
    * Center of the transformation to be applied.
    */
-  CenterTransform = 1 << 8,
+  CenterTransform,
   /**
    * Center of the adorner.
    */
-  Center = 1 << 9,
-  RotateTopLeft = 1 << 10,
-  RotateTopCenter = 1 << 11,
-  RotateTopRight = 1 << 12,
-  RotateBottomLeft = 1 << 13,
-  RotateBottomCenter = 1 << 14,
-  RotateBottomRight = 1 << 15,
-  RotateLeftCenter = 1 << 16,
-  RotateRightCenter = 1 << 17,
+  Center,
+  RotateTopLeft,
+  RotateTopCenter,
+  RotateTopRight,
+  RotateBottomLeft,
+  RotateBottomCenter,
+  RotateBottomRight,
+  RotateLeftCenter,
+  RotateRightCenter,
 }
 
 export class AdornerTypeUtils {
@@ -55,39 +52,39 @@ export class AdornerTypeUtils {
    * Get opposite adorner side if any.
    */
   static getOpposite(handle: AdornerType): AdornerType {
-    if (Utils.bitwiseEquals(handle, AdornerType.RotateTopLeft)) {
+    if (handle === AdornerType.RotateTopLeft) {
       return AdornerType.RotateBottomRight;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateTopCenter)) {
+    } else if (handle === AdornerType.RotateTopCenter) {
       return AdornerType.RotateBottomCenter;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateTopRight)) {
+    } else if (handle === AdornerType.RotateTopRight) {
       return AdornerType.RotateBottomLeft;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateBottomLeft)) {
+    } else if (handle === AdornerType.RotateBottomLeft) {
       return AdornerType.RotateTopRight;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateBottomCenter)) {
+    } else if (handle === AdornerType.RotateBottomCenter) {
       return AdornerType.RotateTopCenter;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateBottomRight)) {
+    } else if (handle === AdornerType.RotateBottomRight) {
       return AdornerType.RotateTopLeft;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateLeftCenter)) {
+    } else if (handle === AdornerType.RotateLeftCenter) {
       return AdornerType.RotateRightCenter;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RotateRightCenter)) {
+    } else if (handle === AdornerType.RotateRightCenter) {
       return AdornerType.RotateLeftCenter;
     }
 
-    if (Utils.bitwiseEquals(handle, AdornerType.TopLeft)) {
+    if (handle === AdornerType.TopLeft) {
       return AdornerType.BottomRight;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.TopCenter)) {
+    } else if (handle === AdornerType.TopCenter) {
       return AdornerType.BottomCenter;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.TopRight)) {
+    } else if (handle === AdornerType.TopRight) {
       return AdornerType.BottomLeft;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.BottomLeft)) {
+    } else if (handle === AdornerType.BottomLeft) {
       return AdornerType.TopRight;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.BottomCenter)) {
+    } else if (handle === AdornerType.BottomCenter) {
       return AdornerType.TopCenter;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.BottomRight)) {
+    } else if (handle === AdornerType.BottomRight) {
       return AdornerType.TopLeft;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.LeftCenter)) {
+    } else if (handle === AdornerType.LeftCenter) {
       return AdornerType.RightCenter;
-    } else if (Utils.bitwiseEquals(handle, AdornerType.RightCenter)) {
+    } else if (handle === AdornerType.RightCenter) {
       return AdornerType.LeftCenter;
     }
 
@@ -102,47 +99,47 @@ export class AdornerTypeUtils {
     const transformPoint = new DOMPoint(bounds.x, bounds.y);
 
     if (
-      Utils.bitwiseEquals(handle, AdornerType.TopLeft) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateTopLeft)
+      handle === AdornerType.TopLeft ||
+      handle === AdornerType.RotateTopLeft
     ) {
       return transformPoint;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.TopCenter) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateTopCenter)
+      handle === AdornerType.TopCenter ||
+      handle === AdornerType.RotateTopCenter
     ) {
       transformPoint.x = bounds.x + bounds.width / 2;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.TopRight) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateTopRight)
+      handle === AdornerType.TopRight ||
+      handle === AdornerType.RotateTopRight
     ) {
       transformPoint.x = bounds.x + bounds.width;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.BottomLeft) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateBottomLeft)
+      handle === AdornerType.BottomLeft ||
+      handle === AdornerType.RotateBottomLeft
     ) {
       transformPoint.x = bounds.x;
       transformPoint.y = bounds.y + bounds.height;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.BottomCenter) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateBottomCenter)
+      handle === AdornerType.BottomCenter ||
+      handle === AdornerType.RotateBottomCenter
     ) {
       transformPoint.x = bounds.x + bounds.width / 2;
       transformPoint.y = bounds.y + bounds.height;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.BottomRight) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateBottomRight)
+      handle === AdornerType.BottomRight ||
+      handle === AdornerType.RotateBottomRight
     ) {
       transformPoint.x = bounds.x + bounds.width;
       transformPoint.y = bounds.y + bounds.height;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.LeftCenter) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateLeftCenter)
+      handle === AdornerType.LeftCenter ||
+      handle === AdornerType.RotateLeftCenter
     ) {
       transformPoint.x = bounds.x;
       transformPoint.y = bounds.y + bounds.height / 2;
     } else if (
-      Utils.bitwiseEquals(handle, AdornerType.RightCenter) ||
-      Utils.bitwiseEquals(handle, AdornerType.RotateRightCenter)
+      handle === AdornerType.RightCenter ||
+      handle === AdornerType.RotateRightCenter
     ) {
       transformPoint.x = bounds.x + bounds.width;
       transformPoint.y = bounds.y + bounds.height / 2;
