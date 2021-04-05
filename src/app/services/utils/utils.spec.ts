be1c27469911cc1dc8b96838107a314ae5b1a63b
Ievgen Naida <evgenynaida@gmail.com>
2021-04-05 19:28:25 +0200

    unittest keepInBounds

--- src/app/services/utils/utils.spec.ts
+++ src/app/services/utils/utils.spec.ts
@@ -12,4 +12,11 @@ describe("Test Utils", () => {
     expect(p.x).toEqual(-5);
     expect(p.y).toEqual(-5);
   });
+
+  it("keep in bounds", () => {
+    expect(Utils.keepInBounds(0.5, 0, 1)).toEqual(0.5);
+    expect(Utils.keepInBounds(-1, 0, 1)).toEqual(0);
+    expect(Utils.keepInBounds(2, 0, 1)).toEqual(1);
+    expect(Utils.keepInBounds(-20, -10, 1)).toEqual(-10);
+  });
 });
